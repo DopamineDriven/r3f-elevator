9a9926f878edf5cac84d7cd84d96cc73fe570ffe
Andrew Ross <andrew.ross@takeda.com>
2025-04-17 23:07:30 -0500

    up

---
+++ utils/__out__/pbr/elegant-stone-tiles.ts
@@ -0,0 +1,14 @@
+export const elegantStoneTiles = {
+  albedo:
+    "https://raw.githubusercontent.com/DopamineDriven/r3f-elevator/refs/heads/master/public/textures/elegant-stone-tiles/elegant-stone-tiles-albedo.png",
+  ao: "https://raw.githubusercontent.com/DopamineDriven/r3f-elevator/refs/heads/master/public/textures/elegant-stone-tiles/elegant-stone-tiles-ao.png",
+  height:
+    "https://raw.githubusercontent.com/DopamineDriven/r3f-elevator/refs/heads/master/public/textures/elegant-stone-tiles/elegant-stone-tiles-height.png",
+  metallic:
+    "https://raw.githubusercontent.com/DopamineDriven/r3f-elevator/refs/heads/master/public/textures/elegant-stone-tiles/elegant-stone-tiles-metallic.png",
+  ogl: "https://raw.githubusercontent.com/DopamineDriven/r3f-elevator/refs/heads/master/public/textures/elegant-stone-tiles/elegant-stone-tiles-normal-ogl.png",
+  preview:
+    "https://raw.githubusercontent.com/DopamineDriven/r3f-elevator/refs/heads/master/public/textures/elegant-stone-tiles/elegant-stone-tiles-preview.jpg",
+  roughness:
+    "https://raw.githubusercontent.com/DopamineDriven/r3f-elevator/refs/heads/master/public/textures/elegant-stone-tiles/elegant-stone-tiles-roughness.png"
+} as const;
