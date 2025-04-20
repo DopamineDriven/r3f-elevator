95017ee0c41ad18e338939dae66f3a0fa7ec2363
Andrew Ross <andrew.ross@takeda.com>
2025-04-20 18:15:49 -0500

    up

--- ui/elevator/r3f/constants/index.ts
+++ ui/elevator/r3f/constants/index.ts
@@ -1,89 +1,120 @@
 import type { PBRTextureSet } from "@/ui/elevator/r3f/types";
 
 export const brushedMetal = {
-  ao: "https://raw.githubusercontent.com/DopamineDriven/r3f-elevator/refs/heads/master/public/textures/enhanced/brushed-metal/brushed-metal-26-94-ao.jpg",
+  ao: "https://asrosscloud.com/textures/enhanced/brushed-metal/brushed-metal-26-94-ao.jpg",
   albedo:
-    "https://raw.githubusercontent.com/DopamineDriven/r3f-elevator/refs/heads/master/public/textures/enhanced/brushed-metal/brushed-metal-26-94-diffuse.jpg",
+    "https://asrosscloud.com/textures/enhanced/brushed-metal/brushed-metal-26-94-diffuse.jpg",
   metalness:
-    "https://raw.githubusercontent.com/DopamineDriven/r3f-elevator/refs/heads/master/public/textures/enhanced/brushed-metal/brushed-metal-26-94-metalness.jpg",
+    "https://asrosscloud.com/textures/enhanced/brushed-metal/brushed-metal-26-94-metalness.jpg",
   normal:
-    "https://raw.githubusercontent.com/DopamineDriven/r3f-elevator/refs/heads/master/public/textures/enhanced/brushed-metal/brushed-metal-26-94-normal.jpg",
+    "https://asrosscloud.com/textures/enhanced/brushed-metal/brushed-metal-26-94-normal.jpg",
   roughness:
-    "https://raw.githubusercontent.com/DopamineDriven/r3f-elevator/refs/heads/master/public/textures/enhanced/brushed-metal/brushed-metal-26-94-roughness.jpg"
+    "https://asrosscloud.com/textures/enhanced/brushed-metal/brushed-metal-26-94-roughness.jpg"
 };
 
 export const elegantStoneTiles = {
     albedo:
-      "https://raw.githubusercontent.com/DopamineDriven/r3f-elevator/refs/heads/master/public/textures/elegant-stone-tiles/elegant-stone-tiles-albedo.png",
-    ao: "https://raw.githubusercontent.com/DopamineDriven/r3f-elevator/refs/heads/master/public/textures/elegant-stone-tiles/elegant-stone-tiles-ao.png",
+      "https://asrosscloud.com/textures/elegant-stone-tiles/elegant-stone-tiles-albedo.png",
+    ao: "https://asrosscloud.com/textures/elegant-stone-tiles/elegant-stone-tiles-ao.png",
     metallic:
-      "https://raw.githubusercontent.com/DopamineDriven/r3f-elevator/refs/heads/master/public/textures/elegant-stone-tiles/elegant-stone-tiles-metallic.png",
+      "https://asrosscloud.com/textures/elegant-stone-tiles/elegant-stone-tiles-metallic.png",
     normal:
-      "https://raw.githubusercontent.com/DopamineDriven/r3f-elevator/refs/heads/master/public/textures/elegant-stone-tiles/elegant-stone-tiles-normal-ogl.png",
+      "https://asrosscloud.com/textures/elegant-stone-tiles/elegant-stone-tiles-normal-ogl.png",
     roughness:
-      "https://raw.githubusercontent.com/DopamineDriven/r3f-elevator/refs/heads/master/public/textures/elegant-stone-tiles/elegant-stone-tiles-roughness.png"
+      "https://asrosscloud.com/textures/elegant-stone-tiles/elegant-stone-tiles-roughness.png"
   },
   stuccoWall = {
     albedo:
-      "https://raw.githubusercontent.com/DopamineDriven/portfolio-2025/master/apps/web/public/textures/smooth-stucco/smooth-stucco-albedo.png",
-    ao: "https://raw.githubusercontent.com/DopamineDriven/portfolio-2025/master/apps/web/public/textures/smooth-stucco/smooth-stucco-ao.png",
+      "https://asrosscloud.com/textures/smooth-stucco/smooth-stucco-albedo.png",
+    ao: "https://asrosscloud.com/textures/smooth-stucco/smooth-stucco-ao.png",
     metalness:
-      "https://raw.githubusercontent.com/DopamineDriven/portfolio-2025/master/apps/web/public/textures/smooth-stucco/smooth-stucco-Metallic.png",
+      "https://asrosscloud.com/textures/smooth-stucco/smooth-stucco-Metallic.png",
     normal:
-      "https://raw.githubusercontent.com/DopamineDriven/portfolio-2025/master/apps/web/public/textures/smooth-stucco/smooth-stucco-Normal-ogl.png",
+      "https://asrosscloud.com/textures/smooth-stucco/smooth-stucco-Normal-ogl.png",
     roughness:
-      "https://raw.githubusercontent.com/DopamineDriven/portfolio-2025/master/apps/web/public/textures/smooth-stucco/smooth-stucco-Roughness.png"
+      "https://asrosscloud.com/textures/smooth-stucco/smooth-stucco-Roughness.png"
   };
 
 // Texture definitions
 export const TEXTURES = {
-  // could use with elevator doors
   brushedMetal: {
     albedo:
-      "https://raw.githubusercontent.com/DopamineDriven/portfolio-2025/master/apps/web/public/textures/brushed-metal/brushed-metal-albedo.png",
-    ao: "https://raw.githubusercontent.com/DopamineDriven/portfolio-2025/master/apps/web/public/textures/brushed-metal/brushed-metal-ao.png",
+      "https://asrosscloud.com/textures/brushed-metal/brushed-metal-albedo.png",
+    ao: "https://asrosscloud.com/textures/brushed-metal/brushed-metal-ao.png",
     metalness:
-      "https://raw.githubusercontent.com/DopamineDriven/portfolio-2025/master/apps/web/public/textures/brushed-metal/brushed-metal-metallic.png",
+      "https://asrosscloud.com/textures/brushed-metal/brushed-metal-metallic.png",
     normal:
-      "https://raw.githubusercontent.com/DopamineDriven/portfolio-2025/master/apps/web/public/textures/brushed-metal/brushed-metal-normal-ogl.png",
+      "https://asrosscloud.com/textures/brushed-metal/brushed-metal-normal-ogl.png",
     roughness:
-      "https://raw.githubusercontent.com/DopamineDriven/portfolio-2025/master/apps/web/public/textures/brushed-metal/brushed-metal-roughness.png"
+      "https://asrosscloud.com/textures/brushed-metal/brushed-metal-roughness.png"
   } satisfies PBRTextureSet,
   elegantStoneTiles: {
-    ao: "https://raw.githubusercontent.com/DopamineDriven/r3f-elevator/refs/heads/master/public/textures/elegant-stone-tiles/elegant-stone-tiles-ao.png",
+    ao: "https://asrosscloud.com/textures/elegant-stone-tiles/elegant-stone-tiles-ao.png",
     albedo:
-      "https://raw.githubusercontent.com/DopamineDriven/r3f-elevator/refs/heads/master/public/textures/elegant-stone-tiles/elegant-stone-tiles-albedo.png",
+      "https://asrosscloud.com/textures/elegant-stone-tiles/elegant-stone-tiles-albedo.png",
     metalness:
-      "https://raw.githubusercontent.com/DopamineDriven/r3f-elevator/refs/heads/master/public/textures/elegant-stone-tiles/elegant-stone-tiles-metallic.png",
+      "https://asrosscloud.com/textures/elegant-stone-tiles/elegant-stone-tiles-metallic.png",
     normal:
-      "https://raw.githubusercontent.com/DopamineDriven/r3f-elevator/refs/heads/master/public/textures/elegant-stone-tiles/elegant-stone-tiles-normal-ogl.png",
+      "https://asrosscloud.com/textures/elegant-stone-tiles/elegant-stone-tiles-normal-ogl.png",
     roughness:
-      "https://raw.githubusercontent.com/DopamineDriven/r3f-elevator/refs/heads/master/public/textures/elegant-stone-tiles/elegant-stone-tiles-roughness.png"
+      "https://asrosscloud.com/textures/elegant-stone-tiles/elegant-stone-tiles-roughness.png"
   } satisfies PBRTextureSet,
   enhancedBrushedMetal: {
-    ao: "https://raw.githubusercontent.com/DopamineDriven/r3f-elevator/refs/heads/master/public/textures/enhanced/brushed-metal/brushed-metal-26-94-ao.jpg",
+    ao: "https://asrosscloud.com/textures/enhanced/brushed-metal/brushed-metal-26-94-ao.jpg",
     albedo:
-      "https://raw.githubusercontent.com/DopamineDriven/r3f-elevator/refs/heads/master/public/textures/enhanced/brushed-metal/brushed-metal-26-94-diffuse.jpg",
+      "https://asrosscloud.com/textures/enhanced/brushed-metal/brushed-metal-26-94-diffuse.jpg",
     metalness:
-      "https://raw.githubusercontent.com/DopamineDriven/r3f-elevator/refs/heads/master/public/textures/enhanced/brushed-metal/brushed-metal-26-94-metalness.jpg",
+      "https://asrosscloud.com/textures/enhanced/brushed-metal/brushed-metal-26-94-metalness.jpg",
     normal:
-      "https://raw.githubusercontent.com/DopamineDriven/r3f-elevator/refs/heads/master/public/textures/enhanced/brushed-metal/brushed-metal-26-94-normal.jpg",
+      "https://asrosscloud.com/textures/enhanced/brushed-metal/brushed-metal-26-94-normal.jpg",
     roughness:
-      "https://raw.githubusercontent.com/DopamineDriven/r3f-elevator/refs/heads/master/public/textures/enhanced/brushed-metal/brushed-metal-26-94-roughness.jpg"
+      "https://asrosscloud.com/textures/enhanced/brushed-metal/brushed-metal-26-94-roughness.jpg"
+  } satisfies PBRTextureSet,
+  paintedPlaster: {
+    ao: "https://asrosscloud.com/textures/painted-plaster/painted_plaster_wall_ao_8k.jpg",
+    metalness:
+      "https://asrosscloud.com/textures/painted-plaster/painted_plaster_wall_arm_8k.jpg",
+    albedo:
+      "https://asrosscloud.com/textures/painted-plaster/painted_plaster_wall_diff_8k.jpg",
+    normal:
+      "https://asrosscloud.com/textures/painted-plaster/painted_plaster_wall_nor_gl_8k.jpg",
+    roughness:
+      "https://asrosscloud.com/textures/painted-plaster/painted_plaster_wall_rough_8k.jpg"
   } satisfies PBRTextureSet,
   stuccoWall: {
     albedo:
-      "https://raw.githubusercontent.com/DopamineDriven/portfolio-2025/master/apps/web/public/textures/smooth-stucco/smooth-stucco-albedo.png",
-    ao: "https://raw.githubusercontent.com/DopamineDriven/portfolio-2025/master/apps/web/public/textures/smooth-stucco/smooth-stucco-ao.png",
+      "https://asrosscloud.com/textures/smooth-stucco/smooth-stucco-albedo.png",
+    ao: "https://asrosscloud.com/textures/smooth-stucco/smooth-stucco-ao.png",
     metalness:
-      "https://raw.githubusercontent.com/DopamineDriven/portfolio-2025/master/apps/web/public/textures/smooth-stucco/smooth-stucco-Metallic.png",
+      "https://asrosscloud.com/textures/smooth-stucco/smooth-stucco-Metallic.png",
     normal:
-      "https://raw.githubusercontent.com/DopamineDriven/portfolio-2025/master/apps/web/public/textures/smooth-stucco/smooth-stucco-Normal-ogl.png",
+      "https://asrosscloud.com/textures/smooth-stucco/smooth-stucco-Normal-ogl.png",
     roughness:
-      "https://raw.githubusercontent.com/DopamineDriven/portfolio-2025/master/apps/web/public/textures/smooth-stucco/smooth-stucco-Roughness.png"
+      "https://asrosscloud.com/textures/smooth-stucco/smooth-stucco-Roughness.png"
+  } satisfies PBRTextureSet,
+  subtleBlackGranite: {
+    albedo:
+      "https://asrosscloud.com/textures/subtle-black-granite/subtle-black-granite-albedo.png",
+    ao: "https://asrosscloud.com/textures/subtle-black-granite/subtle-black-granite-ao.png",
+    metalness:
+      "https://asrosscloud.com/textures/subtle-black-granite/subtle-black-granite-metallic.png",
+    normal:
+      "https://asrosscloud.com/textures/subtle-black-granite/subtle-black-granite-normal-ogl.png",
+    roughness:
+      "https://asrosscloud.com/textures/subtle-black-granite/subtle-black-granite-roughness.png"
+  } satisfies PBRTextureSet,
+  whiteMarble: {
+    albedo:
+      "https://asrosscloud.com/textures/white-marble/white-marble-albedo.png",
+    ao: "https://asrosscloud.com/textures/white-marble/white-marble-ao.png",
+    metalness:
+      "https://asrosscloud.com/textures/white-marble/white-marble-metallic.png",
+    normal:
+      "https://asrosscloud.com/textures/white-marble/white-marble-normal-ogl.png",
+    roughness:
+      "https://asrosscloud.com/textures/white-marble/white-marble-roughness.png"
   } satisfies PBRTextureSet
 } as const;
-
 
 const getIt = <const T extends keyof typeof TEXTURES>(target: T) => {
   return [target, TEXTURES[target]] as const;
@@ -112,5 +143,5 @@ export const ANIMATION_TIMING = {
   BUTTON_PRESS: 391.813,
   DOOR_OPEN: 1848,
   ELEVATOR_SOUND: 1619.592,
-  TRANSITION: 2768.98,
-}
+  TRANSITION: 2768.98
+};
--- utils/generate-obj.ts
+++ utils/generate-obj.ts
@@ -5,7 +5,7 @@ function getIt(argv4 = process.argv[4]) {
   return fs.readDir(`public/textures/${argv4}`).map(v => {
     return [
       v.split(/(-)/g).reverse()[0]?.split(/\./g)?.[0] ?? "",
-      `https://raw.githubusercontent.com/DopamineDriven/r3f-elevator/refs/heads/master/public/textures/${argv4}/${v}`
+      `https://asrosscloud.com/textures/${argv4}/${v}`
     ] as const;
   });
 }
