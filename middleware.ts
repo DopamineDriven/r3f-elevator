f7d1123130a357079a03843ef19bf12f9581684f
Andrew Ross <andrew.ross@takeda.com>
2025-04-21 05:15:43 -0500

    up

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,48 @@
 import type { NextRequest } from "next/server";
 import { NextResponse, userAgent } from "next/server";
 
+const allowedOrigins = ['*']
+
+const corsOptions = {
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, HEAD, OPTIONS',
+  'Access-Control-Allow-Headers': '*',
+}
+
+ function cors(request: NextRequest) {
+  // Check the origin from the request
+  const origin = request.headers.get('origin') ?? ''
+  const isAllowedOrigin = allowedOrigins.includes(origin)
+
+  // Handle preflighted requests
+  const isPreflight = request.method === 'OPTIONS'
+
+  if (isPreflight) {
+    const preflightHeaders = {
+      ...(isAllowedOrigin && { 'Access-Control-Allow-Origin': origin }),
+      ...corsOptions,
+    }
+    return NextResponse.json({}, { headers: preflightHeaders })
+  }
+
+  // Handle simple requests
+  const response = NextResponse.next()
+
+  if (isAllowedOrigin) {
+    response.headers.set('Access-Control-Allow-Origin', origin)
+  }
+
+  Object.entries(corsOptions).forEach(([key, value]) => {
+    response.headers.set(key, value)
+  })
+
+  return response
+}
+
 function detectDeviceAndSetCookies(
   request: NextRequest,
   response: NextResponse
 ) {
+  cors(request);
   const { device, ua } = userAgent(request);
 
   const {hostname} = request.nextUrl;
